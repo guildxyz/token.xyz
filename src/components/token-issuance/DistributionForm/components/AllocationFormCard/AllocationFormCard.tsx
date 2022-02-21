1c688b833b04c8f587d0913b7a3ffbd33a0fa724
BrickheadJohnny <johnny.zgen@gmail.com>
2022-02-21 14:30:14 +0100

    CSV parsing - base

--- src/components/token-issuance/DistributionForm/components/AllocationFormCard/AllocationFormCard.tsx
+++ src/components/token-issuance/DistributionForm/components/AllocationFormCard/AllocationFormCard.tsx
@@ -3,12 +3,18 @@ import {
   FormControl,
   FormErrorMessage,
   GridItem,
+  HStack,
+  Icon,
+  IconButton,
   Input,
   SimpleGrid,
   Tag,
   Text,
 } from "@chakra-ui/react"
-import { useFormContext } from "react-hook-form"
+import { parse } from "papaparse"
+import { TrashSimple } from "phosphor-react"
+import { useRef, useState } from "react"
+import { useFormContext, useWatch } from "react-hook-form"
 import FormCard from "../FormCard"
 import VestingTypePicker from "./components/VestingTypePicker"
 
@@ -19,27 +25,112 @@ type Props = {
 }
 
 const AllocationFormCard = ({ index, field, onRemove }: Props): JSX.Element => {
+  const fileInputRef = useRef(null)
+
   const {
+    control,
     register,
+    setValue,
     formState: { errors },
   } = useFormContext()
+
+  const allocationAddressesAmounts = useWatch({
+    name: `distributionData.${index}.allocationAddressesAmounts`,
+    control,
+  })
+
+  const onUploadClick = () => {
+    if (!fileInputRef?.current) return
+    fileInputRef.current.click()
+  }
+
+  const [isParseLoading, setIsParseLoading] = useState(false)
+
+  const onFileInputChange = (e) => {
+    const file = e.target?.files?.[0]
+    if (!file) return
+
+    setIsParseLoading(true)
+    parse(file, {
+      complete: (results) => {
+        if (results.errors.length) return
+        setIsParseLoading(false)
+        setValue(
+          `distributionData.${index}.allocationAddressesAmounts`,
+          results.data.map(([address, amount]) => ({ address, amount }))
+        )
+      },
+      // error: (error) => {
+      //   console.log("an error occurred", error)
+      // },
+    })
+  }
+
+  const onRemoveCsv = (e) => {
+    if (!fileInputRef?.current) return
+    fileInputRef.current.value = null
+    setValue(`distributionData.${index}.allocationAddressesAmounts`, null)
+  }
 
   return (
     <FormCard title="Allocation" onRemove={onRemove}>
       <SimpleGrid gridTemplateColumns="repeat(2, 1fr)" gap={4}>
-        <FormControl isInvalid={errors?.allocationName} w="full">
+        <FormControl
+          isInvalid={errors?.distributionData?.index?.allocationName}
+          w="full"
+        >
           <Input
             {...register(`distributionData.${index}.allocationName`, {
               required: "This field is required!",
             })}
             placeholder="Name"
           />
-          <FormErrorMessage>{errors?.allocationName?.message}</FormErrorMessage>
+          <FormErrorMessage>
+            {errors?.distributionData?.index?.allocationName?.message}
+          </FormErrorMessage>
         </FormControl>
 
-        <Button colorScheme="primary" h={10}>
-          Upload .csv
-        </Button>
+        <FormControl position="relative">
+          <HStack>
+            <Button
+              colorScheme="primary"
+              w="full"
+              h={10}
+              onClick={onUploadClick}
+              isLoading={isParseLoading}
+              isDisabled={allocationAddressesAmounts?.length || isParseLoading}
+            >
+              {allocationAddressesAmounts?.length
+                ? `${allocationAddressesAmounts.length} addresses`
+                : "Upload .csv"}
+            </Button>
+
+            {allocationAddressesAmounts?.length && (
+              <IconButton
+                aria-label="Remove .csv"
+                icon={<Icon as={TrashSimple} />}
+                colorScheme="red"
+                onClick={onRemoveCsv}
+                w={10}
+                h={10}
+              />
+            )}
+          </HStack>
+
+          <Input
+            ref={fileInputRef}
+            type="file"
+            display="none"
+            position="absolute"
+            top={0}
+            left={0}
+            accept=".csv"
+            onChange={onFileInputChange}
+          />
+          <FormErrorMessage>
+            {errors?.distributionData?.index?.allocationCsv?.message}
+          </FormErrorMessage>
+        </FormControl>
 
         <GridItem colSpan={2}>
           <VestingTypePicker index={index} />
