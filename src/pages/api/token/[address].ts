f23c2663c2ee2dc8696fc5176dd633d6a94ae6e1
BrickheadJohnny <johnny.zgen@gmail.com>
2022-03-12 12:47:07 +0100

    Reworked `token/[address]` endpoint

--- src/pages/api/token/[address].ts
+++ src/pages/api/token/[address].ts
@@ -1,42 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import nextConnect from "next-connect"
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== "GET")
-    return res.status(501).json({
-      message: `Method ${req.method} is not implemented for this endpoint`,
-    })
+const handler = nextConnect({
+  onError(error, _, res: NextApiResponse) {
+    res.status(501).json({ error: `${error.message}` })
+  },
+})
 
-  try {
-    const pinataRes = await fetch(
-      `https://api.pinata.cloud/data/pinList?status=pinned&metadata[name]=${req.query.address}`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.PINATA_ADMIN_JWT}`,
-          "Content-Type": "application/json",
-        },
-      }
+handler.get((req: NextApiRequest, res: NextApiResponse) =>
+  fetch(`${process.env.NEXT_PUBLIC_FLEEK_BUCKET}/${req.query.address}/info.json`)
+    .then((response) =>
+      response.ok
+        ? response.json().then((tokenInfo) => res.json(tokenInfo))
+        : res.status(response.status).json({ error: response.statusText })
     )
-
-    if (!pinataRes.ok)
-      return res.status(pinataRes.status).json({ error: pinataRes.statusText })
-
-    const json = await pinataRes.json()
-
-    if (!json.count) return res.status(404).json({ error: "Not found" })
-
-    const data = json.rows[0]
-    const prettyData = {
-      hash: data.ipfs_pin_hash,
-      name: data.metadata.name,
-      icon: data.metadata.keyvalues?.icon,
-      airdrops: data.metadata.keyvalues?.airdrops?.split(","),
-      vestings: data.metadata.keyvalues?.vestings?.split(","),
-    }
-
-    return res.json(prettyData)
-  } catch (_) {
-    return res.status(500).json({ error: "Could not fetch token data" })
-  }
-}
+    .catch((_) => res.status(500).json({ error: "Could not get files from IPFS" }))
+)
 
 export default handler
