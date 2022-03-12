21009d85a73941a461d62584e7e7ecefc89d9c22
BrickheadJohnny <johnny.zgen@gmail.com>
2022-03-12 13:28:10 +0100

    DistributinPreview: displaying chart conditionally

--- src/components/token-issuance/DistributionPreview.tsx
+++ src/components/token-issuance/DistributionPreview.tsx
@@ -1,5 +1,15 @@
+import { useFormContext, useWatch } from "react-hook-form"
+import { TokenIssuanceFormType } from "types"
 import Chart from "./DistributionForm/components/Chart"
 
-const DistributionPreview = (): JSX.Element => <Chart isSimple />
+const DistributionPreview = (): JSX.Element => {
+  const { control } = useFormContext<TokenIssuanceFormType>()
+  const initialSupply = useWatch({ control, name: "initialSupply" })
+  const maxSupply = useWatch({ control, name: "maxSupply" })
+
+  if (!initialSupply && !maxSupply) return null
+
+  return <Chart isSimple />
+}
 
 export default DistributionPreview
