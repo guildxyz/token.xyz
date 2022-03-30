67d7dcb1b41e08a8ceb73e755ea5a6371b9ea62b
BrickheadJohnny <johnny.zgen@gmail.com>
2022-03-30 17:43:59 +0200

    useTokenXyzContract hook fixes

--- src/hooks/useTokenXyzContract.ts
+++ src/hooks/useTokenXyzContract.ts
@@ -9,7 +9,12 @@ const useTokenXyzContract = (): ethers.Contract => {
   const provider = useProvider()
 
   return useContract({
-    addressOrName: TOKEN_XYZ_CONTRACT[networkData?.chain?.id ?? 3],
+    addressOrName:
+      TOKEN_XYZ_CONTRACT[
+        networkData?.chain?.id && !networkData?.chain?.unsupported
+          ? networkData?.chain?.id
+          : 3
+      ],
     contractInterface: TokenXyzABI,
     signerOrProvider: signerData ?? provider,
   })
